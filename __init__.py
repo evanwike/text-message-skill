e972aebc1fc00154aa0b5cfa9653c6873e8afcfd
Evan W <sportsdude716@gmail.com>
2019-04-07 21:57:55 -0500

    _

--- __init__.py
+++ __init__.py
@@ -5,20 +5,20 @@ from twilio.rest import Client
 
 LOGGER = getLogger(__name__)
 
-account_sid = ''
-auth_token = ''
-# client = Client(account_sid, auth_token)
-phone_num = ''
+account_sid = os.environ['TEXT_MESSAGE_SKILL_SID']
+auth_token = os.environ['TEXT_MESSAGE_SKILL_AUTH']
+client = Client(account_sid, auth_token)
+phone_num = os.environ['TEXT_MESSAGE_SKILL_PHONE_NUM']
 
 class TextMessage(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.numbers = {
-            'evan': '',
-            'cam': '',
-            'dui': '',
-            'max': '',
-            'tondi': ''
+            'evan': '+18163059565',
+            'cam': '+18165166540',
+            'dui': '+17145489619',
+            'max': '+16263846895',
+            'tondi': '+17857645832'
         }
 
     @intent_file_handler('send_message.intent')
